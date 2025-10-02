fbc1c813b5723999529f1c5afe5de3fb2762a56a
Vaibhav Agrawal <98815392+Vaibhav464@users.noreply.github.com>
2025-10-02 18:51:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // FIX: This tells Vite to use the repository name as the base path for assets.
-  base: '/MoleCal/',
   plugins: [react()],
 })
